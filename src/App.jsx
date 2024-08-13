278478f4ecb9cbfc8c610636725742e9fb69cd89
george <george920102@gmail.com>
2024-08-13 10:41:30 +0800

    add Homepage's css in App.css, and add a react-icons package in json

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import React from "react";
 import HelloWorld from "./HelloWorld";
 import "./App.css"
 import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <Home />
+      <HomePage />
       <HelloWorld />
     </>
   );
