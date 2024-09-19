0213d5353cb5527c7b8622644d38d92786438bfe
george <george920102@gmail.com>
2024-09-20 00:54:37 +0800

    update new appearence of navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@
 
 import { FaSearch } from "react-icons/fa";
 import { PiShoppingCartFill } from "react-icons/pi";
+import { BsPersonCircle } from "react-icons/bs";
+import { IoIosNotifications } from "react-icons/io";
 import { NavLink } from "react-router-dom";
 import './Navbar.scss';
 import '../../App.css'
@@ -10,29 +12,29 @@ function Navbar() {
     return (
         <div className="navbar">
             <div className="navbarContainer">
+                
                 <div className="firstLine">
-                    <NavLink to={"/auth/loginpage"}>
-                    <button className="navButtonLogin">登入</button>
-                    </NavLink>
-                    
-                    <NavLink to={"/auth/registerpage"}>
-                    <button className="navButtonRegister">註冊</button>
-                    </NavLink>
-                </div>
-                <div className="secondLine">
                     <div className="left">
                         <NavLink to={"/"}>
                             <span className="logo">GEORGE.BAKERY</span>
                         </NavLink>
                     </div>
-                    <div className="middle">
-                        <input type="search" id="search" name="search" placeholder="請輸入關鍵字或商品名稱" />
-                        <FaSearch id="FaSearch"/>
-                    </div>
                     <div className="right">
-                    <NavLink to={"/auth/registerpage"}>
-                    <button className="btn-hover"><PiShoppingCartFill id="shoppingCartIcon"/> 購物車</button>
-                    </NavLink>
+                        <NavLink to={"/auth/notifications"}>
+                        <button className="btn-hover" title="搜尋"><FaSearch id="searchIcon" /></button>
+                        </NavLink>
+                        <NavLink to={"/auth/notifications"}>
+                        <button className="btn-hover" title="通知"><IoIosNotifications id="notifIcon" /></button>
+                        </NavLink>
+                        <NavLink to={"/auth/shoppingCart"}>
+                        <button className="btn-hover" title="購物車"><PiShoppingCartFill id="shoppingCartIcon" /></button>
+                        </NavLink>
+                        <NavLink to={"/auth/loginpage"}>
+                        <button className="btn-hover" title="會員中心"><BsPersonCircle id="profileIcon" /></button>
+                        </NavLink>
+                        <div>
+                            <p className="username">訪客</p>
+                        </div>
                     </div>
                 </div>
             </div>
