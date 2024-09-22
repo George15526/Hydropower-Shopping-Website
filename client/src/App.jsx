4177c9e8e8759ddccad2d9345b0bec0a4817f390
george <george920102@gmail.com>
2024-09-22 13:39:16 +0800

    update App.jsx for routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./App.css"
 import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/auth/loginpage" element={<LoginPage/>} />
-        <Route path="/auth/registerpage" element={<RegisterPage/>} />
       </Routes>
     </BrowserRouter>
   )
