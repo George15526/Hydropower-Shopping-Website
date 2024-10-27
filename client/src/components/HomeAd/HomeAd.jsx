f5c35ad4104b9ce6f836e7ef988fee1f4f86d45b
george <george920102@gmail.com>
2024-10-27 19:20:06 +0800

    refactor: refactor the HomeAd, and manage the HomeAd's images as a json file

--- client/src/components/HomeAd/HomeAd.jsx
+++ client/src/components/HomeAd/HomeAd.jsx
@@ -1,41 +1,23 @@
-import { Carousel } from "react-bootstrap";
+import { Container, Carousel } from "react-bootstrap";
+import images from "../../data/homeAds.json";
 import './HomeAd.scss';
 
 const HomeAd = () => {
   return (
-    <div className="homeAd">
-        <Carousel>
-            <Carousel.Item className="items">
-                <img
-                    className="pic"
-                    src="src/assets/breadAd_1.jpg"
-                    alt="Slide image alt"
-                />
-            </Carousel.Item>
-            <Carousel.Item className="items">
-                <img
-                    className="pic"
-                    src="src/assets/breadAd_2.jpg"
-                    alt="Slide image alt"
-                />
-            </Carousel.Item>
-            <Carousel.Item className="items">
-                <img
-                    className="pic"
-                    src="src/assets/breadAd_3.jpg"
-                    alt="Slide image alt"
-                />
-            </Carousel.Item>
-            <Carousel.Item className="items">
-                <img
-                    className="pic"
-                    src="src/assets/breadAd_4.jpg"
-                    alt="Slide image alt"
-                />
-            </Carousel.Item>
-        </Carousel>
-    </div>
-  )
-}
+    <Container fluid className="homeAd mx-0 px-5 d-flex justify-content-center">
+      <Carousel className="w-75 px-4">
+        {images.map((image, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-100"
+              src={image.src}
+              alt={image.alt}
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </Container>
+  );
+};
 
 export default HomeAd;
